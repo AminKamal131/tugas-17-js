07d57ae7432ebe289c7f96983f8adaa986383dcd
Amin Kamal <aminkamal355@gmail.com>
2022-12-18 21:37:27 +0800

    tugas 17
    tugas 17

---
+++ tugas17.js
@@ -0,0 +1,31 @@
+function upp(){
+    var up = ("Saya beLajar bahaSa peMrograman deNgan khUsuk.")
+    console.log(up.toLowerCase())
+    console.log(up.toUpperCase())
+
+    console.log("---------------------------------------------------------")
+
+    var pp = ("Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.")
+    console.log(pp.toLowerCase())
+    console.log(pp.toUpperCase())
+}
+upp()
+
+
+console.log('====================================')
+
+console.log('Kode ASCII')
+
+function po() {
+    
+var aski = ("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+console.log(aski.charCodeAt(0))
+console.log(aski.charCodeAt(1))
+console.log(aski.charCodeAt(2))
+console.log(aski.charCodeAt(3))
+console.log(aski.charCodeAt(4))
+console.log(aski.charCodeAt(5))
+}
+
+po()
